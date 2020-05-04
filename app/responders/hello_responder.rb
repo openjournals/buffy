a8f26a1d92bbed2c564e9cd5c51fbb19747a57a6
Juanjo Bazán <jjbazan@gmail.com>
2020-05-04 21:10:00 +0200

    add spec for hello_responder

--- app/responders/hello_responder.rb
+++ app/responders/hello_responder.rb
@@ -4,10 +4,10 @@ class HelloResponder < Responder
 
   def define_listening
     @event_action = "issue_comment.created"
-    @event_regex = /\AHello @#{@bot_name}/i
+    @event_regex = /\AHello @#{@bot_name}\z/i
   end
 
   def process_message(message, context)
-    respond("Hi!", context) if message.match(event_regex)
+    respond("Hi!", context)
   end
 end
---
+++ spec/responders/hello_responder_spec.rb
@@ -0,0 +1,35 @@
+require_relative "../spec_helper.rb"
+
+describe HelloResponder do
+
+  subject do
+    described_class
+  end
+
+  describe "listening" do
+    before { @responder = subject.new({bot_github_user: 'botsci'}, {}) }
+
+    it "should listen to new comments" do
+      expect(@responder.event_action).to eq("issue_comment.created")
+    end
+
+    it "should define regex" do
+      expect(@responder.event_regex).to match("Hello @botsci")
+      expect(@responder.event_regex).to_not match("```Hello @botsci")
+      expect(@responder.event_regex).to_not match("Hello @botsci!")
+    end
+  end
+
+  describe "#process_message" do
+    before do
+      @responder = subject.new({bot_github_user: 'botsci'}, {})
+      disable_github_calls_for(@responder)
+    end
+
+    it "should respond to github" do
+      context = OpenStruct.new({ repo: "buffy/test" })
+      expect(@responder).to receive(:respond).with("Hi!", context)
+      @responder.process_message("Hello @botsci", context)
+    end
+  end
+end
