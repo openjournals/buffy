02b37df70ddf834ecddcddc779778975a30d971a
Juanjo Bazán <jjbazan@gmail.com>
2020-05-04 15:05:09 +0200

    add methods to read/update issues to GitHub module

--- app/lib/github.rb
+++ app/lib/github.rb
@@ -1,11 +1,16 @@
 require 'octokit'
 
 module GitHub
-  # Authenticated Octokit
 
+  # Authenticated Octokit
   def github_client
     @github_client ||= Octokit::Client.new(:access_token => @settings[:gh_access_token],
                                            :auto_paginate => true)
+  end
+
+  # Return an Octokit GitHub Issue
+  def issue(context=@context)
+    @issue ||= github_client.issue(context.repo, context.issue_id)
   end
 
   # Post messages to a GitHub issue.
@@ -18,5 +23,9 @@ module GitHub
   # Context is an OpenStruct created in lib/github_webhook_parser
   def label_issue(labels, context)
     github_client.add_labels_to_an_issue(context.repo, context.issue_id, labels)
+  end
+
+  def update_issue(context, options={})
+    github_client.update_issue(context.repo, context.issue_id, options)
   end
 end
