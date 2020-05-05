15aea6a83094cbe4706a739f14419caa40f1f416
Juanjo Bazán <jjbazan@gmail.com>
2020-05-05 11:27:04 +0200

    spec for responder class

---
+++ spec/responder_spec.rb
@@ -0,0 +1,76 @@
+require_relative "./spec_helper.rb"
+
+describe Responder do
+
+  subject do
+    app = described_class.new({}, {})
+  end
+
+  before do
+    subject.event_action = "test_created"
+    subject.event_regex = /\Atesting\z/
+  end
+
+  describe "#responds_on?" do
+    it "should be true if there is not event_action defined" do
+      subject.event_action = nil
+      expect(subject.responds_on?(OpenStruct.new({ event_action: "whatever" }))).to be_truthy
+    end
+
+    it "should be true for the defined event_action" do
+      expect(subject.responds_on?(OpenStruct.new({ event_action: "test_created" }))).to be_truthy
+    end
+
+    it "should be false for other event_actions" do
+      expect(subject.responds_on?(OpenStruct.new({ event_action: "test_edited" }))).to be_falsey
+      expect(subject.responds_on?(OpenStruct.new({ event_action: "" }))).to be_falsey
+      expect(subject.responds_on?(OpenStruct.new({ event_action: nil }))).to be_falsey
+    end
+  end
+
+  describe "#responds_to?" do
+    it "should be true if there is not event_regex defined" do
+      subject.event_regex = nil
+      expect(subject.responds_to?("whatever")).to be_truthy
+    end
+
+    it "should be true if message matches the event_regex" do
+      expect(subject.responds_to?("testing")).to be_truthy
+    end
+
+    it "should be false when messages don't match the event_regex" do
+      expect(subject.responds_to?("test")).to be_falsey
+      expect(subject.responds_to?("testing again")).to be_falsey
+      expect(subject.responds_to?("" )).to be_falsey
+      expect(subject.responds_to?(nil )).to be_falsey
+    end
+  end
+
+  describe "#call" do
+    it "should not process message if responds_on? is false" do
+      allow(subject).to receive(:responds_on?).and_return(false)
+      allow(subject).to receive(:responds_to?).and_return(true)
+      allow(subject).to receive(:process_message).never
+      expect(subject.call("testing", {})).to be false
+    end
+
+
+    it "should not process message if responds_to? is false" do
+      allow(subject).to receive(:responds_on?).and_return(true)
+      allow(subject).to receive(:responds_to?).and_return(false)
+      allow(subject).to receive(:process_message).never
+      expect(subject.call("testing", {})).to be false
+    end
+
+    it "should process message if responds_on? and responds_to? are both true" do
+      context = OpenStruct.new({ event_action: "test_created" })
+      message = "testing"
+      allow(subject).to receive(:responds_on?).and_return(true)
+      allow(subject).to receive(:responds_to?).and_return(true)
+      allow(subject).to receive(:process_message).and_return(true)
+
+      expect(subject).to receive(:process_message).once.with(message, context)
+      expect(subject.call(message, context)).to be true
+    end
+  end
+end
