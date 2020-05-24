9f26eead945d0565625f18cf03323fcff537b0ef
Juanjo Bazán <jjbazan@gmail.com>
2020-05-24 12:37:05 +0200

    add hi

--- spec/responders/hello_responder_spec.rb
+++ spec/responders/hello_responder_spec.rb
@@ -15,6 +15,7 @@ describe HelloResponder do
 
     it "should define regex" do
       expect(@responder.event_regex).to match("Hello @botsci")
+      expect(@responder.event_regex).to match("hi @botsci")
       expect(@responder.event_regex).to_not match("```Hello @botsci")
       expect(@responder.event_regex).to_not match("Hello @botsci!")
     end
