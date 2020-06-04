23ba15aaef1e89b4f459c8e253170030c37030ca
Juanjo Bazán <jjbazan@gmail.com>
2020-06-04 12:29:07 +0200

    index is master_doc

--- docs/conf.py
+++ docs/conf.py
@@ -35,6 +35,8 @@ extensions = [
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
+# The master toctree document.
+master_doc = 'index'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
