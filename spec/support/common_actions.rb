a396667f4ae58040a5c4e8fdb90e43660c867e37
Juanjo Bazán <jjbazan@gmail.com>
2020-06-12 14:40:40 +0200

    disable template_urls calls

--- spec/support/common_actions.rb
+++ spec/support/common_actions.rb
@@ -16,6 +16,7 @@ module CommonActions
 
   def disable_github_calls_for(responder)
     allow(responder).to receive(:issue).and_return(true)
+    allow(responder).to receive(:template_url).and_return(true)
     allow(responder).to receive(:bg_respond).and_return(true)
     allow(responder).to receive(:label_issue).and_return(true)
     allow(responder).to receive(:update_issue).and_return(true)
