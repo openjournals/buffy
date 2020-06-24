8a6ac3ad8f3c8d8423a65247c9766a1467affa54
Juanjo Bazán <jjbazan@gmail.com>
2020-06-24 13:08:01 +0200

    add default params for testing the close issue command responder

--- spec/support/responder_params.rb
+++ spec/support/responder_params.rb
@@ -3,7 +3,8 @@ module ResponderParams
     n = rand(1e5)
     params_by_responder = {
       SetValueResponder => { name: "set_value_#{n}"},
-      LabelCommandResponder => { command: "label_command_#{n}", labels: ["label_#{n}"] }
+      LabelCommandResponder => { command: "label_command_#{n}", labels: ["label_#{n}"] },
+      CloseIssueCommandResponder => { command: "close_command_#{n}", labels: ["label_#{n}"] }
     }
 
     params_by_responder[responder_class] || {}
