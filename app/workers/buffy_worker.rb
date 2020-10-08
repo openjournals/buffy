a3d2f54f014f7f1c90cd2359f73e0cf7fcabd193
Juanjo Bazán <jjbazan@gmail.com>
2020-10-08 13:37:55 +0200

    add external service worker

--- app/workers/buffy_worker.rb
+++ app/workers/buffy_worker.rb
@@ -1,5 +1,6 @@
 require 'yaml'
 require 'erb'
+require 'faraday'
 
 require_relative '../lib/defaults'
 require_relative '../lib/github'
--- app/workers/external_service_worker.rb
+++ app/workers/external_service_worker.rb
@@ -1,5 +1,34 @@
 class ExternalServiceWorker < BuffyWorker
+
   def perform(service, locals)
-    # Faraday call service[:url]
+    load_context_and_settings(locals)
+
+    http_method = service[:method] || 'post'
+    url = service[:url]
+    template = nil
+
+    query_parameters = service[:query_params] || {}
+    service_mapping = service[:mapping] || {}
+    mapped_parameters = {}
+
+    service[:mapping].each_pair do |k, v|
+      mapped_parameters[k] = locals.delete(v)
+    end
+
+    parameters = {}.merge(query_parameters, mapped_parameters, locals)
+
+    if http_method.downcase == 'get'
+      response = Faraday.get(url, parameters)
+    else
+      headers = {"Content-Type" => "application/json", 'Accept' => 'application/json'}
+      response = Faraday.post(url, parameters.to_json, headers)
+    end
+
+    if service[:template_file]
+      parsed_response = JSON.parse(response.body)
+      respond_external_template(service[:template_file], parsed_response)
+    else
+      respond(response)
+    end
   end
 end
