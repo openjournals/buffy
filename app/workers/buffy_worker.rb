969bc83d4f01b6f4c12ac000592bb3fc0c26529f
Juanjo Bazán <jjbazan@gmail.com>
2020-10-08 13:36:30 +0200

    allow workers to use templating

--- app/workers/buffy_worker.rb
+++ app/workers/buffy_worker.rb
@@ -1,10 +1,35 @@
+require 'yaml'
+require 'erb'
+
+require_relative '../lib/defaults'
 require_relative '../lib/github'
+require_relative '../lib/templating'
 
 class BuffyWorker
   include Sidekiq::Worker
   include GitHub
+  include Templating
+
+  def rack_environment
+    ENV['RACK_ENV'] || "test"
+  end
 
   def path
     "tmp/#{jid}"
+  end
+
+  def load_context_and_settings(config)
+    @context = OpenStruct.new(
+      issue_id: config[:issue_id],
+      repo: config[:repo],
+    )
+
+    document = ERB.new(IO.read("../../config/settings-#{rack_environment}.yml")).result
+    yaml = YAML.load(document)
+    buffy_settings = yaml[:buffy]
+
+    @settings = {}
+    @settings[:templates_path] = buffy_settings[:templates_path] || default_settings[:templates_path]
+    @settings[:gh_access_token] = buffy_settings[:gh_access_token] || default_settings[:gh_access_token]
   end
 end
