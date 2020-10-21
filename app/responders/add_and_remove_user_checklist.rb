c023ae7c6fd7304785cdc6de46226700e804b170
Juanjo Bazán <jjbazan@gmail.com>
2020-10-21 12:27:47 +0200

    add user checklists responder

---
+++ app/responders/add_and_remove_user_checklist.rb
@@ -0,0 +1,60 @@
+require_relative '../lib/responder'
+
+class AddAndRemoveUserChecklistResponder < Responder
+
+  def define_listening
+    required_params :template_file
+
+    @event_action = "issue_comment.created"
+    @event_regex = /\A@#{@bot_name} (add|remove) checklist for (\S+)\s*\z/i
+  end
+
+  def process_message(message)
+    add_or_remove = @match_data[1].downcase
+    user = @match_data[2]
+
+    @mark = "<!--checklist-for-#{user}-->"
+    @end_mark = "<!--end-checklist-for-#{user}-->"
+
+    @previous = read_from_body(@mark, @end_mark)
+
+    if add_or_remove == "add"
+      add_checklist user
+    elsif add_or_remove == "remove"
+      remove_checklist user
+    end
+  end
+
+  def add_checklist(user)
+    if @previous.empty?
+      checklist = "\n" + @mark +
+                  "\n" + "## Review checklist for " + user +
+                  "\n" + render_external_template(template_file, locals) +
+                  "\n" + @end_mark+ "\n"
+
+      append_to_body checklist
+      respond("Checklist added for #{user}")
+    else
+      respond("There is already a checklist for #{user}")
+    end
+  end
+
+  def remove_checklist(user)
+    if @previous.empty?
+      respond("There is not a checklist for #{user}")
+    else
+      delete_from_body(@mark, @end_mark, true)
+      respond("Checklist for #{user} removed")
+    end
+  end
+
+  def description
+    ["Add review checklist for a user",
+     "Remove the checklist for a user"]
+  end
+
+  def example_invocation
+    ["@#{@bot_name} add checklist for @username",
+     "@#{@bot_name} remove checklist for @username"]
+  end
+end
