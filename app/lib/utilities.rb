9bc656051b7a9c4a1472e2ca6091e324fc10d130
Juanjo Bazán <jjbazan@gmail.com>
2020-10-23 13:16:29 +0200

    add repo cloning and branch utilities

--- app/lib/utilities.rb
+++ app/lib/utilities.rb
@@ -1,4 +1,5 @@
 require 'find'
+require 'open3'
 
 module Utilities
 
@@ -45,6 +46,19 @@ module Utilities
     end
 
     paper_path
+  end
+
+  def clone_repo(url, local_path)
+    url = URI.extract(url.to_s).first
+    return false if url.nil?
+
+    stdout, stderr, status = Open3.capture3 "git clone #{url} #{local_path}"
+    status.success?
+  end
+
+  def change_branch(branch, local_path)
+    stdout, stderr, status = Open3.capture3 "git -C #{local_path} checkout #{branch}"
+    status.success?
   end
 
 end
--- spec/utilities_spec.rb
+++ spec/utilities_spec.rb
@@ -49,4 +49,46 @@ describe "Utilities" do
     end
   end
 
+  describe "#clone_repo" do
+    it "should return true when succesfully cloned a repo to a local path" do
+      expect(Open3).to receive(:capture3).
+                       with("git clone http:/repository-url.com ./local/folder").
+                       and_return(["OK", "", OpenStruct.new(success?: true)])
+      expect(subject.clone_repo("http:/repository-url.com", "./local/folder")).to be_truthy
+    end
+
+    it "should return false if url is invalid or empty" do
+      expect(Open3).to_not receive(:capture3)
+
+      expect(subject.clone_repo("No valid url here", "./local/folder")).to be_falsy
+      expect(subject.clone_repo("", "./local/folder")).to be_falsy
+      expect(subject.clone_repo(nil, "./local/folder")).to be_falsy
+    end
+
+    it "should return false if cloning fails" do
+      expect(Open3).to receive(:capture3).
+                       with("git clone http://www.wrong-url.com ./local/folder").
+                       and_return(["", "Invalid URL", OpenStruct.new(success?: false)])
+      expect(subject.clone_repo("http://www.wrong-url.com", "./local/folder")).to be_falsy
+    end
+  end
+
+  describe "#change_branch" do
+    it "should checkout branch and return true" do
+      expect(Open3).to receive(:capture3).
+                       with("git -C local/folder checkout paper-branch").
+                       and_return(["OK", "", OpenStruct.new(success?: true)])
+
+      expect(subject.change_branch("paper-branch", "local/folder")).to be_truthy
+    end
+
+    it "should return false if command fails" do
+      expect(Open3).to receive(:capture3).
+                       with("git -C local/folder checkout newbranch").
+                       and_return(["", "No such file or directory", OpenStruct.new(success?: false)])
+
+      expect(subject.change_branch("newbranch", "local/folder")).to be_falsy
+    end
+  end
+
 end
