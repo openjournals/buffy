384e085e755e917f61e46d9247c6860178f152e0
Juanjo Bazán <jjbazan@gmail.com>
2020-11-04 13:30:24 +0100

    move methods out of utilities
    only used for DOI suggestion so they belong in the doi checker class

--- app/lib/utilities.rb
+++ app/lib/utilities.rb
@@ -3,38 +3,6 @@ require 'open3'
 
 module Utilities
 
-  # How different are two strings?
-  # https://en.wikipedia.org/wiki/Levenshtein_distance
-  def levenshtein_distance(s, t)
-    m = s.length
-    n = t.length
-
-    return m if n == 0
-    return n if m == 0
-    d = Array.new(m+1) {Array.new(n+1)}
-
-    (0..m).each {|i| d[i][0] = i}
-    (0..n).each {|j| d[0][j] = j}
-    (1..n).each do |j|
-      (1..m).each do |i|
-        d[i][j] = if s[i-1] == t[j-1]  # adjust index into string
-                    d[i-1][j-1]       # no operation required
-                  else
-                    [ d[i-1][j]+1,    # deletion
-                      d[i][j-1]+1,    # insertion
-                      d[i-1][j-1]+1,  # substitution
-                    ].min
-                  end
-      end
-    end
-
-    d[m][n]
-  end
-
-  def similar?(string_1, string_2)
-    levenshtein_distance(string_1, string_2) < 3
-  end
-
   def find_paper_file(search_path)
     paper_path = nil
 
--- spec/utilities_spec.rb
+++ spec/utilities_spec.rb
@@ -15,28 +15,6 @@ describe "Utilities" do
     disable_github_calls_for(subject)
   end
 
-  describe "#levenshtein_distance" do
-    it "should measuring the difference between strings" do
-      expect(subject.levenshtein_distance("Hello", "Hello")).to eq 0
-      expect(subject.levenshtein_distance("Hello", "hello")).to eq 1
-      expect(subject.levenshtein_distance("low data", "raw data")).to eq 2
-      expect(subject.levenshtein_distance("Sunday", "Saturday")).to eq 3
-    end
-  end
-
-  describe "#similar?" do
-    it "should be true if levenshtein distance is less than 3" do
-      expect(subject.similar?("Hello", "Hello")).to be_truthy
-      expect(subject.similar?("Hello", "hello")).to be_truthy
-      expect(subject.similar?("low data", "raw data")).to be_truthy
-    end
-
-    it "should be false if levenshtein distance is 3 or bigger" do
-      expect(subject.similar?("Sunday", "Saturday")).to be_falsy
-      expect(subject.similar?("Way too", "different")).to be_falsy
-    end
-  end
-
   describe "#find_paper_file" do
     it "should return paper path if present" do
       allow(Find).to receive(:find).with("/repo/path/").and_return(["lib/papers", "./docs/paper.md", "app"])
