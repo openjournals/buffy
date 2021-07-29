60d28181de45220ffcf443a3be877a828c7837a6
Juanjo Bazán <jjbazan@gmail.com>
2021-07-29 12:57:34 +0200

    return empty array

--- spec/support/common_actions.rb
+++ spec/support/common_actions.rb
@@ -19,7 +19,7 @@ module CommonActions
     allow(responder).to receive(:template_url).and_return("")
     allow(responder).to receive(:bg_respond).and_return(true)
     allow(responder).to receive(:label_issue).and_return(true)
-    allow(responder).to receive(:issue_labels).and_return(true)
+    allow(responder).to receive(:issue_labels).and_return([])
     allow(responder).to receive(:unlabel_issue).and_return(true)
     allow(responder).to receive(:update_issue).and_return(true)
     allow(responder).to receive(:close_issue).and_return(nil)
