5c889a28a9daaaf3fcb9bd34cbe5bb3a0f00e190
Juanjo Bazán <jjbazan@gmail.com>
2022-01-03 13:08:57 +0100

    add reviewers list responder

---
+++ app/responders/reviewers_list_responder.rb
@@ -0,0 +1,86 @@
+require_relative '../lib/responder'
+
+class ReviewersListResponder < Responder
+
+  keyname :reviewers_list
+
+  def define_listening
+    @event_action = "issue_comment.created"
+    @event_regex = /\A@#{bot_name} (add|remove) (\S+) (to reviewers|from reviewers|as reviewer)\.?\s*\z/i
+  end
+
+  def process_message(message)
+    add_or_remove = @match_data[1].downcase
+    new_reviewer = @match_data[2].strip
+    to_or_from = @match_data[3].downcase
+
+    if !issue_body_has?("reviewers-list")
+      respond("I can't find the reviewers list")
+      return
+    end
+
+    unless username?(new_reviewer) || new_reviewer == "me"
+      respond("I can't add that reviewer: #{new_reviewer} is not a username")
+      return
+    end
+
+    new_reviewer = "@#{context.sender}" if new_reviewer == "me"
+
+    add_to_or_remove_from = [add_or_remove, to_or_from].join(" ")
+
+    if ["add to reviewers", "add as reviewer"].include?(add_to_or_remove_from)
+      add new_reviewer
+    elsif ["remove from reviewers", "remove as reviewer"].include?(add_to_or_remove_from)
+      remove new_reviewer
+    else
+      respond("That command is confusing. Did you mean to ADD TO REVIEWERS or to REMOVE FROM REVIEWERS?")
+    end
+  end
+
+  def add(new_reviewer)
+    if list_of_reviewers.include?(new_reviewer)
+      respond("#{new_reviewer} is already included in the reviewers list")
+    else
+      new_list = (list_of_reviewers + [new_reviewer]).uniq.join(", ")
+      update_value("reviewers-list", new_list)
+      respond("#{new_reviewer} added to the reviewers list!")
+      add_collaborator(new_reviewer) if add_as_collaborator?(new_reviewer)
+      add_assignee(new_reviewer) if add_as_assignee?(new_reviewer)
+      process_labeling if list_of_reviewers.empty?
+    end
+  end
+
+  def remove(reviewer)
+    if list_of_reviewers.include?(reviewer)
+      new_list = (list_of_reviewers - [reviewer]).uniq.join(", ")
+      update_value("reviewers-list", new_list)
+      respond("#{reviewer} removed from the reviewers list!")
+      remove_assignee(reviewer) if add_as_assignee?(reviewer)
+      process_reverse_labeling if new_list.empty?
+    else
+      respond("#{reviewer} is not in the reviewers list")
+    end
+  end
+
+  def list_of_reviewers
+    @list_of_reviewers ||= read_value_from_body("reviewers-list").split(",").map(&:strip)
+  end
+
+  def add_as_collaborator?(value)
+    username?(value) && params[:add_as_collaborator] == true
+  end
+
+  def add_as_assignee?(value)
+    username?(value) && params[:add_as_assignee] == true
+  end
+
+  def description
+    ["Add to this issue's reviewers list",
+     "Remove from this issue's reviewers list"]
+  end
+
+  def example_invocation
+    ["@#{bot_name} add #{params[:sample_value] || '@username'} as reviewer",
+     "@#{bot_name} remove #{params[:sample_value] || '@username'} from reviewers"]
+  end
+end
---
+++ spec/responders/reviewers_list_responder_spec.rb
@@ -0,0 +1,264 @@
+require_relative "../spec_helper.rb"
+
+describe ReviewersListResponder do
+
+  subject do
+    described_class
+  end
+
+  describe "listening" do
+    before { @responder = subject.new({env: {bot_github_user: "botsci"}}, {}) }
+
+    it "should listen to new comments" do
+      expect(@responder.event_action).to eq("issue_comment.created")
+    end
+
+    it "should define regex" do
+      expect(@responder.event_regex).to match("@botsci add @arfon to reviewers")
+      expect(@responder.event_regex).to match("@botsci add @arfon as reviewer")
+      expect(@responder.event_regex).to match("@botsci add me as reviewer")
+      expect(@responder.event_regex).to match("@botsci remove me from reviewers")
+      expect(@responder.event_regex).to match("@botsci remove @arfon from reviewers  \r\n")
+      expect(@responder.event_regex).to_not match("@botsci add to reviewers")
+      expect(@responder.event_regex).to_not match("@botsci remove   from reviewers")
+    end
+  end
+
+  describe "#process_message" do
+    before do
+      @responder = subject.new({env: {bot_github_user: "botsci"}}, {})
+      @responder.context = double(sender: "editor")
+      disable_github_calls_for(@responder)
+    end
+
+    describe "adding new reviewer" do
+      before do
+        @msg = "@botsci add @xuanxu as reviewer"
+        @responder.match_data = @responder.event_regex.match(@msg)
+
+        issue_body = "...Reviewers: <!--reviewers-list-->@arfon<!--end-reviewers-list--> ..."
+        allow(@responder).to receive(:issue_body).and_return(issue_body)
+      end
+
+      it "should add new user to the reviewers list in the body of the issue" do
+        expected_new_body = "...Reviewers: <!--reviewers-list-->@arfon, @xuanxu<!--end-reviewers-list--> ..."
+        expect(@responder).to receive(:update_issue).with({ body: expected_new_body })
+        @responder.process_message(@msg)
+      end
+
+      it "should respond to github" do
+        expect(@responder).to receive(:respond).with("@xuanxu added to the reviewers list!")
+        @responder.process_message(@msg)
+      end
+
+      it "should accept to or as syntax" do
+        msg = "@botsci add @xuanxu to reviewers"
+        @responder.match_data = @responder.event_regex.match(msg)
+        expected_new_body = "...Reviewers: <!--reviewers-list-->@arfon, @xuanxu<!--end-reviewers-list--> ..."
+        expect(@responder).to receive(:update_issue).with({ body: expected_new_body })
+        @responder.process_message(msg)
+      end
+
+      it "should accept 'me' as the new reviewer" do
+        msg = "@botsci add me as reviewer"
+        @responder.match_data = @responder.event_regex.match(msg)
+        expected_new_body = "...Reviewers: <!--reviewers-list-->@arfon, @editor<!--end-reviewers-list--> ..."
+        expect(@responder).to receive(:update_issue).with({ body: expected_new_body })
+        expect(@responder).to receive(:respond).with("@editor added to the reviewers list!")
+        @responder.process_message(msg)
+      end
+
+      it "should not add user if already present in the reviewers list" do
+        msg = "@botsci add @arfon to reviewers"
+        @responder.match_data = @responder.event_regex.match(msg)
+        expect(@responder).to_not receive(:update_issue)
+        expect(@responder).to receive(:respond).with("@arfon is already included in the reviewers list")
+        @responder.process_message(msg)
+      end
+
+      it "should not add as assignee/collaborator if not configured" do
+        expect(@responder).to_not receive(:add_collaborator)
+        expect(@responder).to_not receive(:add_assignee)
+        @responder.process_message(@msg)
+      end
+
+      it "should add as collaborator if configured" do
+        @responder.params[:add_as_collaborator] = true
+        expect(@responder).to receive(:add_collaborator)
+        expect(@responder).to_not receive(:add_assignee)
+        @responder.process_message(@msg)
+      end
+
+      it "should add as assignee if configured" do
+        @responder.params[:add_as_assignee] = true
+        expect(@responder).to_not receive(:add_collaborator)
+        expect(@responder).to receive(:add_assignee)
+        @responder.process_message(@msg)
+      end
+    end
+
+    describe "removing a reviewer" do
+      before do
+        @msg = "@botsci remove @xuanxu from reviewers"
+        @responder.match_data = @responder.event_regex.match(@msg)
+
+        issue_body = "...Reviewers: <!--reviewers-list-->@arfon, @xuanxu<!--end-reviewers-list--> ..."
+        allow(@responder).to receive(:issue_body).and_return(issue_body)
+      end
+
+      it "should remove user from the reviewers list in the body of the issue" do
+        expected_new_body = "...Reviewers: <!--reviewers-list-->@arfon<!--end-reviewers-list--> ..."
+        expect(@responder).to receive(:update_issue).with({ body: expected_new_body })
+        @responder.process_message(@msg)
+      end
+
+      it "should accept to or as syntax" do
+        msg = "@botsci remove @xuanxu as reviewer"
+        @responder.match_data = @responder.event_regex.match(msg)
+        expected_new_body = "...Reviewers: <!--reviewers-list-->@arfon<!--end-reviewers-list--> ..."
+        expect(@responder).to receive(:update_issue).with({ body: expected_new_body })
+        @responder.process_message(msg)
+      end
+
+      it "should respond to github" do
+        expect(@responder).to receive(:respond).with("@xuanxu removed from the reviewers list!")
+        @responder.process_message(@msg)
+      end
+
+      it "should not remove username if not present in the reviewers list" do
+        msg = "@botsci remove @other_user from reviewers"
+        @responder.match_data = @responder.event_regex.match(msg)
+        expect(@responder).to_not receive(:update_issue)
+        expect(@responder).to receive(:respond).with("@other_user is not in the reviewers list")
+        @responder.process_message(msg)
+      end
+
+      it "should accept 'me' as the reviewer to remove" do
+        msg = "@botsci remove me from reviewers"
+        @responder.match_data = @responder.event_regex.match(msg)
+        @responder.context = double(sender: "xuanxu")
+        expected_new_body = "...Reviewers: <!--reviewers-list-->@arfon<!--end-reviewers-list--> ..."
+        expect(@responder).to receive(:update_issue).with({ body: expected_new_body })
+        expect(@responder).to receive(:respond).with("@xuanxu removed from the reviewers list!")
+        @responder.process_message(msg)
+      end
+
+      it "should not remove as assignee if not configured" do
+        expect(@responder).to_not receive(:remove_assignee)
+        @responder.process_message(@msg)
+      end
+
+      it "should remove as assignee if configured" do
+        @responder.params[:add_as_assignee] = true
+        expect(@responder).to receive(:remove_assignee)
+        @responder.process_message(@msg)
+      end
+    end
+
+    describe "labeling" do
+      it "should process labeling when adding the first reviewer" do
+        msg = "@botsci add @xuanxu to reviewers"
+        @responder.match_data = @responder.event_regex.match(msg)
+        issue_body = "...Reviewers: <!--reviewers-list--><!--end-reviewers-list--> ..."
+        allow(@responder).to receive(:issue_body).and_return(issue_body)
+
+        expect(@responder).to receive(:process_labeling)
+        @responder.process_message(msg)
+      end
+
+      it "should process reverse labeling when removing the last reviewer" do
+        msg = "@botsci remove @xuanxu from reviewers"
+        @responder.match_data = @responder.event_regex.match(msg)
+        issue_body = "...Reviewers: <!--reviewers-list-->@xuanxu<!--end-reviewers-list--> ..."
+        allow(@responder).to receive(:issue_body).and_return(issue_body)
+
+        expect(@responder).to receive(:process_reverse_labeling)
+        @responder.process_message(msg)
+      end
+
+      it "should not add/remove labels otherwise" do
+        issue_body = "...Reviewers: <!--reviewers-list-->@xuanxu, @arfon<!--end-reviewers-list--> ..."
+        allow(@responder).to receive(:issue_body).and_return(issue_body)
+
+        msg = "@botsci add @user3 to reviewers"
+        @responder.match_data = @responder.event_regex.match(msg)
+
+        expect(@responder).to_not receive(:process_labeling)
+        expect(@responder).to_not receive(:process_reverse_labeling)
+        @responder.process_message(msg)
+
+        msg = "@botsci remove @xuanxu from reviewers"
+        @responder.match_data = @responder.event_regex.match(msg)
+
+        expect(@responder).to_not receive(:process_labeling)
+        expect(@responder).to_not receive(:process_reverse_labeling)
+        @responder.process_message(msg)
+      end
+    end
+  end
+
+  describe "#add_as_collaborator?" do
+    before { @responder = subject.new({env: {bot_github_user: "botsci"}}, {}) }
+
+    it "is false if value is not a username" do
+      expect(@responder.username?("not username value")).to be_falsy
+      expect(@responder.add_as_collaborator?("not username value")).to be_falsy
+    end
+
+    it "is false if param[:add_as_collaborator] is false" do
+      expect(@responder.username?("@username")).to be_truthy
+      expect(@responder.params[:add_as_collaborator]).to be_falsy
+      expect(@responder.add_as_collaborator?("@username")).to be_falsy
+    end
+
+    it "is true if value is username and param[:add_as_collaborator] is true" do
+      expect(@responder.username?("@username")).to be_truthy
+      @responder.params[:add_as_collaborator] = true
+      expect(@responder.add_as_collaborator?("@username")).to be_truthy
+    end
+  end
+
+  describe "#add_as_assignee?" do
+    before { @responder = subject.new({env: {bot_github_user: "botsci"}}, {}) }
+
+    it "is false if value is not a username" do
+      expect(@responder.username?("not username value")).to be_falsy
+      expect(@responder.add_as_assignee?("not username value")).to be_falsy
+    end
+
+    it "is false if param[:add_as_assignee] is false" do
+      expect(@responder.username?("@username")).to be_truthy
+      expect(@responder.params[:add_as_assignee]).to be_falsy
+      expect(@responder.add_as_assignee?("@username")).to be_falsy
+    end
+
+    it "is true if value is username and param[:add_as_assignee] is true" do
+      expect(@responder.username?("@username")).to be_truthy
+      @responder.params[:add_as_assignee] = true
+      expect(@responder.add_as_assignee?("@username")).to be_truthy
+    end
+  end
+
+  describe "documentation" do
+    before do
+      @responder = subject.new({env: {bot_github_user: "botsci"}}, { sample_value: "@new_reviewer"})
+    end
+
+    it "#description should include adding and removing reviewers" do
+      expect(@responder.description[0]).to eq("Add to this issue's reviewers list")
+      expect(@responder.description[1]).to eq("Remove from this issue's reviewers list")
+    end
+
+    it "#example_invocation should use custom sample value if present" do
+      expect(@responder.example_invocation[0]).to eq("@botsci add @new_reviewer as reviewer")
+      expect(@responder.example_invocation[1]).to eq("@botsci remove @new_reviewer from reviewers")
+    end
+
+    it "#example_invocation should have default sample value" do
+      @responder.params = {}
+      expect(@responder.example_invocation[0]).to eq("@botsci add @username as reviewer")
+      expect(@responder.example_invocation[1]).to eq("@botsci remove @username from reviewers")
+    end
+  end
+
+end
