06bd21018285ffd0ff12cbd156bfacd887b909c8
Juanjo Bazán <jjbazan@gmail.com>
2022-01-28 10:11:13 +0100

    respond to @whedon mentioning the new bot name

---
+++ app/responders/openjournals/whedon_responder.rb
@@ -0,0 +1,28 @@
+require_relative '../../lib/responder'
+
+module Openjournals
+  class WhedonResponder < Responder
+    keyname :whedon
+
+    def define_listening
+      @event_action = "issue_comment.created"
+      @event_regex = /\A@whedon /i
+    end
+
+    def process_message(message)
+      respond "My name is now @#{bot_name}"
+    end
+
+    def description
+      "My name is not Whedon"
+    end
+
+    def example_invocation
+      "@whedon whatever"
+    end
+
+    def hidden?
+      true
+    end
+  end
+end
