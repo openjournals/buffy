4bfaa53b3bc1d982a6a711478cc37cc77dc349b9
Juanjo Bazán <jjbazan@gmail.com>
2022-02-24 12:36:31 +0100

    allow forward slashes in branch name

--- spec/responders/repo_checks_responder_spec.rb
+++ spec/responders/repo_checks_responder_spec.rb
@@ -20,6 +20,7 @@ describe RepoChecksResponder do
       expect(@responder.event_regex).to match("@botsci check repository")
       expect(@responder.event_regex).to match("@botsci check repository    \r\n")
       expect(@responder.event_regex).to match("@botsci check repository from branch custom-branch")
+      expect(@responder.event_regex).to match("@botsci check repository from branch custom/branch")
       expect(@responder.event_regex).to match("@botsci check repository from branch development    \r\n")
       expect(@responder.event_regex).to_not match("@botsci check repository from branch ")
     end
