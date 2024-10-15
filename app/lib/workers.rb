87a632cd8cd9ee3513068344c42cab02daec438c
Juanjo Bazán <jjbazan@gmail.com>
2024-10-15 22:41:52 +0200

    Configure Sidekiq->SSL->Redis
    https://devcenter.heroku.com/articles/connecting-heroku-redis#connecting-from-sidekiq

--- app/lib/workers.rb
+++ app/lib/workers.rb
@@ -1,4 +1,18 @@
 require 'sidekiq'
 
+Sidekiq.configure_server do |config|
+  config.redis = {
+    url: ENV["REDIS_URL"],
+    ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
+  }
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = {
+      url: ENV["REDIS_URL"],
+      ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
+  }
+end
+
 require "#{File.expand_path '../..', __FILE__}/workers/buffy_worker.rb"
 Dir["#{File.expand_path '../..', __FILE__}/workers/**/*.rb"].sort.each { |f| require f }
